fc47abec5fd194905edbff5edab3bc4738914e98
wolfgunblood <jalajdorai1@gmail.com>
2023-09-10 16:47:10 +0530

    Buttons and description in Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs/app-beta";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  console.log(userId);
   return (
-    <Button>
-      Click Me!!
-    </Button>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">AI PDF Analyzer</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Analyze</Button>}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professinals to instantly
+            anwer questions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <Button>Analyze</Button>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
